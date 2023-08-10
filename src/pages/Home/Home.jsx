8751071434a6af51258e5ddfb8ce210df04a2694
iagoocoelho <iagoocoelho@gmail.com>
2023-08-10 20:24:34 -0300

    removendo

--- src/pages/Home/Home.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { MainContainer } from "components/container/mainContainer";
-
-export const Home = () => {
-  return <MainContainer></MainContainer>;
-};
-
-export default Home;
