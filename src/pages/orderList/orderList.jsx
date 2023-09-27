750a108398050fde6e623be70c9f36346ef6d9c9
iagoocoelho <iagoocoelho@gmail.com>
2023-09-26 22:04:46 -0300

    ajuste

--- src/pages/orderList/orderList.jsx
+++ src/pages/orderList/orderList.jsx
@@ -1,23 +1,64 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { Col } from "react-bootstrap";
-import { MainContainer } from "components/container/mainContainer";
-import StartOrder from "pages/startOrder/startOrder";
+import { Table } from "react-bootstrap";
+import * as ordersActions from "store/orders/actions";
+import { useNavigate } from "react-router-dom";
 
-export const OrderList = () => {
-  // TO DO: Request API de lita de pedidos para iniciar
+export const OrderList = ({ getOrderListRequest, ordersList }) => {
+  let navigate = useNavigate();
+  useEffect(() => {
+    getOrderListRequest();
+  }, [getOrderListRequest]);
 
   return (
-    <MainContainer>
-      <Col className="py-4 title">
-        <h3>Lista de pedidos</h3>
-      </Col>
-
-      <StartOrder />
-      <StartOrder />
-      <StartOrder />
-    </MainContainer>
+    <>
+      {!ordersList.loading && ordersList.success && (
+        <Table>
+          <thead>
+            <tr>
+              <th>Fornecedor</th>
+              <th>Codigo do Fabricante</th>
+              <th>Descrição</th>
+            </tr>
+          </thead>
+          <tbody>
+            {ordersList.data.map((item) => {
+              return (
+                <React.Fragment key={item.id}>
+                  <tr>
+                    <td>{item.clienteFornecedor.nome}</td>
+                    <td>{item.dataEntrega}</td>
+                    <td>
+                      <button
+                        className="btn-blue"
+                        onClick={() => navigate(`/editar-produto/${item.id}`)}
+                      >
+                        Editar
+                      </button>
+                    </td>
+                  </tr>
+                </React.Fragment>
+              );
+            })}
+          </tbody>
+        </Table>
+      )}
+    </>
   );
 };
 
-export default OrderList;
+const mapStateToProps = (state) => {
+  return {
+    ordersList: state.orders.list,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getOrderListRequest: () => {
+      dispatch(ordersActions.getOrderListRequest());
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(OrderList);
